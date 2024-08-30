44ee8000f67c0c3e1dc1b4e0467a7628e76b6247
CaioCesarFaria <321caioster@gmail.com>
2024-08-30 12:22:28 -0300

    Ajustes finais

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const btn_buy = document.getElementById('#btn_comprar');
+
+btn_buy.addEventListener('click', () => {
+    
+    // Redireciona para a página desejada
+    window.location.href = 'https://celcash.celcoin.com.br/landingpage6368244/vivendodepersonal'; // Substitua por sua URL
+});
